f0ef0b16eacb0eed20086d00a83f6f4df3e78ae3
Matt Schell <mschell@doitt.nyc.gov>
2020-10-30 15:50:19 -0400

    adds initial dumb gdbeat scripts

---
+++ gdbeat.bat
@@ -0,0 +1,4 @@
+REM execute from ArcGIS Pro conda environment
+REM environmental SDEFILE file should point to geodatabase administrator (SDE.sde) 
+REM set SDEFILE=T:\GIS\Internal\Connections\oracle19c\dev\GIS-ditGSdv1\mschell_private\sde.sde
+c:\Progra~1\ArcGIS\Pro\bin\Python\scripts\propy.bat .\src\py\gdbeat.py "mschell@doitt.nyc.gov"
