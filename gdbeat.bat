e57bdb0e3410c55a2f252c4d555b4702f665d1a2
Matt Schell <mschell@doitt.nyc.gov>
2020-11-27 15:44:28 -0500

    finalizes gdbeat

--- gdbeat.bat
+++
@@ -1,4 +0,0 @@
-REM execute from ArcGIS Pro conda environment
-REM environmental SDEFILE file should point to geodatabase administrator (SDE.sde) 
-REM set SDEFILE=T:\GIS\Internal\Connections\oracle19c\dev\GIS-ditGSdv1\mschell_private\sde.sde
-c:\Progra~1\ArcGIS\Pro\bin\Python\scripts\propy.bat .\src\py\gdbeat.py "mschell@doitt.nyc.gov"
